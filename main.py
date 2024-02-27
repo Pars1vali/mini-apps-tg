042e2a346b23b46ddf5d0cad69a762ad3a056989
Chumak Vladimir <parsival427@gmail.com>
2024-02-27 20:26:40 +0300

    20:03 27.02

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ html_code = """
 const tg = window.Telegram.WebApp;
 tg.MainButton.show();
 tg.MainButton.setText("Оплатить");
+tg.MainButton.onClick(() => {
+tg.showAlert("Успех");
+}
 alert("Hello from JavaScript!");
 </script>
 """
@@ -29,10 +32,10 @@ st.markdown(html_code, unsafe_allow_html=True)
 
 if image is not None:
     image = np.array(bytearray(image.read()), dtype=np.uint8)
-    cv2_img = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    gray_image = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(cv2_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    # cv2_img = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    # gray_image = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+    # faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(cv2_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    st.image(cv2_img)
+    # st.image(cv2_img)
