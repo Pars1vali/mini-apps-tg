3cf873a537239b9bcb631ad91f208bf1ae3e2b0f
Chumak Vladimir <parsival427@gmail.com>
2024-02-27 20:03:32 +0300

    20:03 27.02

--- main.py
+++ main.py
@@ -1,64 +1,38 @@
 import  streamlit as st
-import streamlit.components.v1 as components
+import cv2, numpy as np
 
+st.write("Старт")
+image = st.camera_input("Сфокать")
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+html_code = """
+<p>Что-то</p>
+<style>
+* {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 200;
+    color: var(--tg-theme-text-color);
+    background: var(--tg-theme-bg-color);
+}
+}
 
-st.title("Составить оплату")
-name = st.text_input("Название покупки")
-desciption = st.text_input("Описаниие")
-payload = st.text_input("Долнительные парметры")
-currency = st.selectbox("Валюта", options=["rub","usd"])
-components.html("""
-    <!DOCTYPE html>
-        <html lang="en">
+</style>
+<script src="https://telegram.org/js/telegram-web-app.js"></script>
+<script>
+const tg = window.Telegram.WebApp;
+tg.MainButton.show();
+tg.MainButton.setText("Оплатить");
+alert("Hello from JavaScript!");
+</script>
+"""
 
-<head>
-    <script src="https://telegram.org/js/telegram-web-app.js">
-    </script>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-    * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    }
-    
-    body {
-        font-family: 'Montserrat', sans-serif;
-        font-weight: 200;
-        color: var(--tg-theme-text-color);
-        background: var(--tg-theme-bg-color);
-    }
-    </style>
-    
-</head>
-<body>
-   <script>
-    const tg = window.Telegram.WebApp;
-    let url = 'https://d5dip6pritbe7tmoain3.apigw.yandexcloud.net/aibot';
-    
-    tg.MainButton.show();
-    tg.MainButton.setText("Оплатить");
-    
-    tg.MainButton.onClick(() => {
-        tg.showAlert("ff");
-    }
-    
-    const sendDataToTelegram = () => {
-        window.Telegram.WebApp.sendData("selectedRegions");
-    }
+st.markdown(html_code, unsafe_allow_html=True)
 
-      useEffect(() => {
-        window.Telegram.WebApp.onEvent('mainButtonClicked', sendDataToTelegram)
-        return () => {
-          window.Telegram.WebApp.offEvent('mainButtonClicked', sendDataToTelegram)
-        }
-      }, [sendDataToTelegram])
-    
-   </script>
+if image is not None:
+    image = np.array(bytearray(image.read()), dtype=np.uint8)
+    cv2_img = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    gray_image = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(cv2_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-</body>
-</html>
-""")
-image_pic = st.camera_input("Камера мен")
+    st.image(cv2_img)
