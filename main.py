899ba6137052906ba1d6db6cd6ac7872c467cabb
Chumak Vladimir <parsival427@gmail.com>
2024-02-28 11:56:16 +0300

    html

--- main.py
+++ main.py
@@ -20,16 +20,21 @@ html("""
     <title>Document</title>
 </head>
 <body>
-<p>Что-то</p>
+<p>Что-то-x1</p>
 <button id="myButton">Нажми меня</button>
     <script>
         document.getElementById("myButton").addEventListener("click", function() {
           alert("Вы нажали кнопку!");
           tg.showAlert("Реально нажали");
         });
+        
         const tg = window.Telegram.WebApp;
         tg.MainButton.setText("Оплатить");
         tg.MainButton.show();
+        
+        tg.MainButton.onClick(() => {
+            alert("Вы нажали кнопку tg!");
+        });
     </script>
 </div>
 </body>
