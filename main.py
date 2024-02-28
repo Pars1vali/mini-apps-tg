700f9e942d131633538c0644d69fc9f620859b00
Chumak Vladimir <parsival427@gmail.com>
2024-02-28 14:28:48 +0300

    add google autfec

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
+from auth import *
 
 st.camera_input("Сделай фотку")
+st.page_link(get_login_str(), label="Войти в Google аккаунт")
+if st.button("display user"):
+    display_user()
