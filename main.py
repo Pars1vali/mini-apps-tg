ab893725863447089be7e5e1a128e6e4a8575a9b
Chumak Vladimir <parsival427@gmail.com>
2024-02-28 10:38:59 +0300

    js

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import streamlit as st
+st.markdown(
+    """
+    <div id="div"></div>
+     <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <script>
+    const tg = window.Telegram.WebApp;
+    tg.MainButton.show();
+    tg.MainButton.setText("Оплатить");
+    </script>
+    """,
+    unsafe_allow_html=True,
+)
