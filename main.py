324917ac0631affb6eaffe6c6c5b686b2e8815f9
Chumak Vladimir <parsival427@gmail.com>
2024-02-28 10:53:52 +0300

    js

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import streamlit as st
-st.markdown(
-    """
-    <div id="div"></div>
-     <script src="https://telegram.org/js/telegram-web-app.js"></script>
-    <script>
-    const tg = window.Telegram.WebApp;
-    tg.MainButton.show();
-    tg.MainButton.setText("Оплатить");
-    </script>
-    """,
-    unsafe_allow_html=True,
-)
+from streamlit.components.v1 import html
+
+# Define your javascript
+tg_src = "https://telegram.org/js/telegram-web-app.js"
+my_js = """
+alert("Hola mundo");
+const tg = window.Telegram.WebApp;
+tg.MainButton.show();
+tg.MainButton.setText("Оплатить");
+"""
+
+# Wrapt the javascript as html code
+my_html = f"<script src={tg_src}></script><script>{my_js}</script>"
+
+# Execute your app
+st.title("Javascript example")
+html(my_html)
