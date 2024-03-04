24aa252df1d5f9258f253891b53c908d982ea0c1
Chumak Vladimir <parsival427@gmail.com>
2024-03-04 17:34:49 +0300

    <feature(photo)>: add upload photo

--- main.py
+++ main.py
@@ -1,48 +1,86 @@
-import streamlit as st
-import requests, json
-from streamlit.components.v1 import html
-# from pyzbar.pyzbar import decode
-# # from PIL import Image
-# pyzbar==0.1.9
-# pillow==10.2.0
+from PIL import Image
+from io import BytesIO
+import requests, json, io, gzip, base64, streamlit as st
 
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-def send(promo_code):
+def sendPromoRequest(mode, promo_code):
     url = "https://d5dip6pritbe7tmoain3.apigw.yandexcloud.net/aibot"
-
     headers = {'Content-Type': 'application/json',
                'X-Custom-Info': 'check_promo'}
 
     data_json = json.dumps({
         'promo_code': promo_code
     })
+
     response = requests.post(url=url, data=data_json, headers=headers)
     return response.text
-qr = st.toggle("Промокод/QR-код")
+def sendImageServer(image_base64, name):
+    url = "https://d5dip6pritbe7tmoain3.apigw.yandexcloud.net/aibot"
+    headers = {'Content-Type': 'application/json',
+               'X-Custom-Info': 'image_upload'}
+
+    data_json = json.dumps({
+        'name': name,
+        'photo': image_base64
+    })
+
+    response = requests.post(url=url, data=data_json, headers=headers)
+    return response.text
+def sendRequestNameImges():
+    url = "https://d5dip6pritbe7tmoain3.apigw.yandexcloud.net/aibot"
+    headers = {'Content-Type': 'application/json',
+               'X-Custom-Info': 'get_name_image'}
+
+    response = requests.post(url=url, headers=headers)
+    return response.text
+def requestImageServer(name):
+    url = "https://d5dip6pritbe7tmoain3.apigw.yandexcloud.net/aibot"
+    headers = {'Content-Type': 'application/json',
+               'X-Custom-Info': 'image_request'}
+
+    data_json = json.dumps({
+        'name': name
+    })
+
+    response = requests.post(url=url, data=data_json, headers=headers)
+    return response.text
+
+qr = st.toggle("Просмотреть/Загрузить")
+
 
 if qr:
-    image = st.camera_input("Сфоткай QR код")
-    # html("""<script>alert("f");</script>""")
+    name_image = st.text_input("Введите название для картинки")
+    image = st.camera_input("Сделай фотку")
 
     if image is not None:
-        # decocdeQR = decode(Image.open(image))
-        # st.write(decocdeQR[0].data.decode('ascii'))
-        st.write("dd")
+        image_bytes = image.read()
+        image_base64 = base64.b64encode(image_bytes).decode("utf-8")
+
+        st.write(image_base64)
+        image_d_base64 = sendImageServer(image_base64, name_image)
+        st.write(image_d_base64)
+
+        image_d_bytes = base64.b64decode(image_base64)
+        image_d = Image.open(io.BytesIO(image_d_bytes))
+        st.image(image_d)
+
 else:
-    promo_code = st.text_input("Промокод")
-    send_btn = st.button("Проверить промокод")
+    response = json.loads(sendRequestNameImges())
+    if response is not None:
+        select_image = st.selectbox("Выбери фотографию", response)
+        if select_image is not None:
+            st.write(f"Выбранная фотография - {select_image[0]}")
+            images_json = json.loads(requestImageServer(select_image[0]))
+            image_base64 = images_json[0][0]
+            image_data_str = image_base64[2:-1]
+            image_data = base64.b64decode(image_data_str)
+            image = Image.open(io.BytesIO(image_data))
+            st.image(image)
 
-    if send_btn:
-        if promo_code=="":
-            st.warning("Введите промокод")
-        else:
-            responce = send(promo_code)
-            st.caption("Промокод на скидку")
-            st.title(f"{responce.split(' ')[1]} - "
-                     f"{responce.split(' ')[0]}%")
-
+    else:
+        st.info("Фотографии не загружены")
 
 
 
